57550a6af052787350bb4fc42dfe7b69648d00d1
erickadikah <erickadikah@gmail.com>
2022-10-28 09:16:21 -0700

    new add

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,19 @@ import json
 from uuid import uuid4
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwars.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "__class_":
+                        continue
+                    setattr(self, key, value)
+            else:
+                self.id = str(uuid4())
+                self.created_at = datetime.today()
+                self.updated_at = datetime.today()
+                storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
