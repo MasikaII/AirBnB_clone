758094e2d6db79963db0128ea7811c3d01861bc0
erickadikah <erickadikah@gmail.com>
2022-10-30 15:15:52 -0700

    storage

--- models/base_model.py
+++ models/base_model.py
@@ -36,17 +36,19 @@ class BaseModel:
         updates the public instance attribute
         updated_at with the current datetime
         """
-
+        from models import storage
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
         returns a dictionary containing all
         keys/values of __dict__ of the instance
         """
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
+        dict_1 = self.__dict__.copy()
+        dict_1["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if k in ("created_at", "updated_at"):
+                v = self.__dict__[k].isoformat()
+                dict_1[k] = v
+        return dict_1
