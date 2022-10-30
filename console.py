09338ebda34eeb00104864099afcb3028c63f817
erickadikah <erickadikah@gmail.com>
2022-10-30 14:37:12 -0700

    code code code

--- console.py
+++ console.py
@@ -6,27 +6,28 @@ import cmd
 class HBNBCommand(cmd.Cmd):
 
     '''This is a class definition for the command processor'''
-    prompt = '(hbnb)'
-
-    def do_quit(self, line):
-        '''This is a method to exit the program'''
-        return True
+    prompt = "(hbnb)"
 
     def do_EOF(self, line):
         '''This is a method to exit the command line'''
+        return True
+
+    def help_EOF(self):
+        """this command quits the program"""
+        print("This also quits program\n")
+
+     def do_quit(self, line):
+        '''This is a method to exit the program'''
         return True
 
     def help_quit(self):
         '''Give information on the method quit'''
         print("Quit command to exit the program")
 
-    def help_EOF(self):
-        """this command quits the program"""
-        return True
-
     def empty_line(self):
         """ when the line is empty print nothing"""
         pass
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
