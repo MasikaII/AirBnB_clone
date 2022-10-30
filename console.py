dd23dbf4c64d20bac03efee5e0e177632ef8a6ae
erickadikah <erickadikah@gmail.com>
2022-10-30 14:45:12 -0700

    new change

--- console.py
+++ console.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""contains the entry point of a command interpreter"""
+"""Contains the entry point of a command interpreter"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-
-    '''This is a class definition for the command processor'''
-    prompt = "(hbnb)"
+    """This is the class definition for the command interpreter"""
+    prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        '''This is a method to exit the command line'''
+        """ This is a method to exit the program"""
         return True
 
     def help_EOF(self):
-        """this command quits the program"""
+        """ This helps to quit the program """
         print("This also quits program\n")
 
     def do_quit(self, line):
-        '''This is a method to exit the program'''
+        """This is a method to quit the program"""
         return True
 
     def help_quit(self):
-        '''Give information on the method quit'''
-        print("Quit command to exit the program")
+        """Gives more information on the method quit"""
+        print("Quit command to exit the program\n")
 
-    def empty_line(self):
-        """ when the line is empty print nothing"""
+    def emptyline(self):
+        """when line is empty print nothing"""
         pass
 
 
