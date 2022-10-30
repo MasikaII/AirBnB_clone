7daa3475b07d04e892c4cdcad9509b2b046e2465
erickadikah <erickadikah@gmail.com>
2022-10-30 12:40:41 -0700

    me

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key == "__class_":
                         continue
-                setattr(self, key, value)
+                t mergesetattr(self, key, value)
             else:
                 self.id = str(uuid4())
                 self.created_at = datetime.today()
