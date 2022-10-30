5f716c80991d1583e4973ff78f1e98f54c6a9657
erickadikah <erickadikah@gmail.com>
2022-10-30 15:02:42 -0700

    base

--- models/base_model.py
+++ models/base_model.py
@@ -6,37 +6,37 @@ A module containing a base class
 
 from datetime import datetime
 from uuid import uuid4
-import json
-import models
 
 
 class BaseModel:
-    """The base class that defines all common attributes for other classes"""
+    """
+    A base class that defines all common attributes for other classes
+    """
 
     def __init__(self, *args, **kwargs):
         """instantiates the public class attributes"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "__class__":
-                    continue
-                setattr(self, key, value)
-        else:
+        if not kwargs:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.created_at = self.updatye_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """prints the string representation of the class"""
-        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
         updates the public instance attribute
         updated_at with the current datetime
         """
+
         self.updated_at = datetime.now()
         models.storage.save()
 
